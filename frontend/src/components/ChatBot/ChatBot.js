249c89e3c44cbd8e683b326a4d83fbf351c645d2
Farah Azeem <farah.sarfaraz16@gmail.com>
2024-11-11 10:59:33 +1100

    ChatBot added to the App..

---
+++ frontend/src/components/ChatBot/ChatBot.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import classes from "./chatBot.module.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+const systemMessage = {
+  role: "system",
+  content: "Explain all concepts like I am 10 years old.", //Explain things like you're talking to a software professional with 2 years of experience.
+};
+
+export default function ChatBot({ closeChat }) {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am a bot. How can I help you?",
+      sentTime: "just now",
+      sender: "ChatGPT",
+      direction: "incoming",
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      sender: "user",
+      direction: "outgoing",
+    };
+
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+    setTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return {
+        role: role,
+        content: messageObject.message,
+      };
+    });
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_CHATBOT_API_KEY}`,
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data.choices[0].message.content);
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: "ChatGPT",
+            direction: "incoming",
+          },
+        ]);
+        setTyping(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  return (
+    <>
+      <div className={classes.chatbotHeader}>
+        <h3>ChatBot</h3>
+        <button onClick={() => closeChat(false)}>x</button>
+      </div>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList
+            scrollBehavior="smooth"
+            typingIndicator={
+              typing ? <TypingIndicator content="Chatting with Bot" /> : null
+            }
+          >
+            {messages.map((message, index) => {
+              return <Message key={index} model={message} />;
+            })}
+          </MessageList>
+          <MessageInput placeholder="Type message here" onSend={handleSend} />
+        </ChatContainer>
+      </MainContainer>
+    </>
+  );
+}
